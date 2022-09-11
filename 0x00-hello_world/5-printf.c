105cb65ab0052037a00485334d1cf46b479fe38a
crespalawal <cosmasobi93@gmail.com>
2022-09-11 13:06:01 -0700

    task 5 stdout with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - prints string with printf
+ * Return: 0 for success, 1 otherwise
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
