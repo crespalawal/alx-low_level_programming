ea6ed1bd2db6a8e1b827aa33b290fbd756570cf7
crespalawal <cosmasobi93@gmail.com>
2022-09-11 11:02:25 -0700

    adjusting for betty compliance

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main(void)
+/*
+ *  main entry point takes no args
+ * the program prints to the std output using puts functions instead of printf
+ * it returns success(0) always
+ */
+int main(void) /*main function*/
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 
-	return 0;
+
+	return (0);
 }
