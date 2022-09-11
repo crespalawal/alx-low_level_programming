cc93a7bde3d88c3658c84497c0fbaa75d01fa926
crespalawal <cosmasobi93@gmail.com>
2022-09-11 12:41:12 -0700

    task 4 print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * main - entry point takes no args
- * Description: the program prints to the std output using p * uts instead of printf
+ * Description: the program prints to the std output
+ * using puts instead of printf
  * Return:  success(0) always
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 
 	return (0);
