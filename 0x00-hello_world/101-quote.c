31b8961e311459849696ecb7288ae4351f865bb6
crespalawal <cosmasobi93@gmail.com>
2022-09-11 15:39:05 -0700

    print using write(STDOUT_FILENO)

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - prints error to Stderr
+ * Description: prints a quote followed by a newline
+ * Stderr
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	write(STDOUT_FILENO,  "and that
+	piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
