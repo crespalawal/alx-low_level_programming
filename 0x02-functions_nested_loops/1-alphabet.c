5773b34a3fa873acfb0819578023c6097685d5c1
crespalawal <cosmasobi93@gmail.com>
2022-09-13 21:49:09 -0700

    print abc in lowercase then newline

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - to print a-z
+ * Return: always 0 success
+ */
+
+int print_alphabet(void)
+{
+	char c;
+	for (c = "a", c <= "z", c++)
+	{
+		_putchar(c);
+	}
+_putchar("\n");
