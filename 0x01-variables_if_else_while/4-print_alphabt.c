24f6d96af5ac021697cd9954545ab0fc97b63585
crespalawal <cosmasobi93@gmail.com>
2022-09-15 12:21:49 -0700

    alpha except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+	char e = 'e';
+	char q = 'q';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+		putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
