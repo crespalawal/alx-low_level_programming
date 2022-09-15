b56ffe27c089a1f71155738d210aedc050765c48
crespalawal <cosmasobi93@gmail.com>
2022-09-15 11:26:09 -0700

    lower then upper alpha, newline

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <=  'z')
+	{
+		putchar(lower);
+		lower++;
+
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+
+	}
+	putchar('\n');
+		return (0);
+}
