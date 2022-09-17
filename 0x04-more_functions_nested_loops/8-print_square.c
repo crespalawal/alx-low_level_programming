40c3232fac37db404a9c1c4f5cadc9adc131c79a
crespalawal <cosmasobi93@gmail.com>
2022-09-17 04:44:23 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - draws square with #
+ * @n: (size) integer
+ * Return: nothing
+ */
+
+void print_square(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				putchar('#');
+			}
+			putchar('#');
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
