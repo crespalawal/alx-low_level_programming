281ad058d22fd85ed6d3b07aa3528a09bf16f55d
crespalawal <cosmasobi93@gmail.com>
2022-09-16 23:13:59 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - functions that print all numbers fro * m 0 to 9
+ * Return: returns 0
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
