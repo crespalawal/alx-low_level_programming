9d27499bb6783c876e2bfc7cfb3bb7ea90743470
crespalawal <cosmasobi93@gmail.com>
2022-09-17 03:24:37 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14
+ * Returns: nothing
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				putchar(j / 10 + '0');
+			} putchar(j % 10 + '0');
+		}
+		putchar('\n');
+	}
+}
