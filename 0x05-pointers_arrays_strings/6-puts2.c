cd8544dc80fb68c912e18160b510eb6cd0604fad
crespalawal <cosmasobi93@gmail.com>
2022-09-19 23:13:45 -0700

    6-puts2.c count 1st string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - puts function
+ * @str: parameter
+ */
+void puts2(char *str)
+{
+	int len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
