14b8af4a81f2fc6f7fc1655cb273f54a788204f9
crespalawal <cosmasobi93@gmail.com>
2022-10-10 07:49:06 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,9 +14,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == 0)
-	return;
-	d->.name = name;
-	d->.age = age;
-	d->.owner = owner;
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
 
