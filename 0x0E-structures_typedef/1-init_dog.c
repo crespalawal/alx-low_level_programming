a8a38149c03577de950191589a645a5103ad031c
crespalawal <cosmasobi93@gmail.com>
2022-10-10 07:14:28 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - structure type
+ * @name: char member1
+ * @age: 2nd member
+ * @owner: 3rd member
+ * @d: pointer to structure
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	while (d == 0)
+	return;
+	*(d).name = name;
+	*(d).age = age;
+	*(d).owner = owner;
+	break;
+}
+
