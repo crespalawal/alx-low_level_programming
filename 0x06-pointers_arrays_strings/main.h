dc36e20ed9b5fc21d51e1fdb2ed1029c7f80ae8f
crespalawal <cosmasobi93@gmail.com>
2022-11-14 15:05:27 +0100

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,5 +17,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *add_strings(char *n1, char *n2, char *r, int *_index);
-
+int _putchar(char c);
 #endif
