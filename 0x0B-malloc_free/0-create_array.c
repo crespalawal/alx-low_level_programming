6c51995b1609576f7eba7f336df9463cb1597453
crespalawal <cosmasobi93@gmail.com>
2022-11-14 17:37:33 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,11 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+
 	for (index = 0; index < size; index++)
-		c = array[index];
+		array[index] = c;
+
 	return (array);
 }
